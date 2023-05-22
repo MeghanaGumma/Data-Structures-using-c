f7905ca388028edaa9aac6f2fe5dce04514ae713
MeghanaGumma <125593046+MeghanaGumma@users.noreply.github.com>
2023-05-22 20:36:35 +0530

    Add files via upload

---
+++ alphadigspc.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main()
+{
+	char ch;
+	scanf("%c",&ch);
+	if((ch>='A'&&ch<='Z')||(ch>='a'&&ch<='z'))
+	{
+		printf("Alphabet");
+	}
+	else if(ch>='0'&&ch<='9')
+	{
+		printf("Digit");
+	}
+	else
+	{
+		printf("Special character");
+	}
+}
+
